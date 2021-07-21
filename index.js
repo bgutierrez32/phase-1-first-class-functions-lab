8de624dead86030899ba779348d8000736d4f619
Bryan Gutierrez <gutierrezbryan32@yahoo.com>
2021-07-21 12:28:00 -0400

    commit tree 44ad2476071ef77d81dcab743fd4aae64f4214c3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob','Freddy', 'Claudia'];
+function returnFirstTwoDrivers(){
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(){
+    return drivers.slice(2, 4);
+}
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(Fare){
+    return function(multi){
+        return multi*Fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers();
+}
+function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers();
+}
